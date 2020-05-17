daa711369d4e4f100088580ce24349109ae49372
kurtdariel <kurtdariel@gmail.com>
2020-05-17 22:13:20 +0800

    Some changes

--- public/script.js
+++ public/script.js
@@ -2,24 +2,3 @@ var scroll = new SmoothScroll('a[href*="#"]',{
     speed: 500,
       speedAsDuration: true
     });
-
-    var cursor = {
-    delay: 8,
-    _x: 0,
-    _y: 0,
-    endX: (window.innerWidth / 2),
-    endY: (window.innerHeight / 2),
-    cursorVisible: true,
-    cursorEnlarged: false,
-    $dot: document.querySelector('.cursor-dot'),
-    $outline: document.querySelector('.cursor-dot-outline'),
-    
-    init: function() {
-        // Set up element sizes
-        this.dotSize = this.$dot.offsetWidth;
-        this.outlineSize = this.$outline.offsetWidth;
-        
-        this.setupEventListeners();
-        this.animateDotOutline();
-    },
-}
